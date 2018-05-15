08a73840ba8a20edb685a0ac0d6baf888eb1ff6b
Xiao Hu <hx9865@126.com>
2018-05-15 14:26:34 -0400

    better

--- django_test/settings.py
+++ django_test/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'wuw1(7_8x8z1=#-4mb^u_rx_t5d&pm)^*27uji38j_y!9t*n#!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
